33c4c3c6e732b5e3b684707e0f3aa5eca2744a30
huanghuiyu <huanghuiyu@yidian-inc.com>
2019-04-19 00:05:16 +0800

    deploy test

--- .umirc.js
+++ .umirc.js
@@ -2,7 +2,7 @@
 // ref: https://umijs.org/config/
 export default {
   history: 'hash',
-  // base:'/var/lib/jenkins/workspace/deploy_penda/',
+  base:'/var/lib/jenkins/workspace/deploy_penda/',
   treeShaking: true,
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
